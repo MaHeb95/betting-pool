93acaa98021ef83cd4f0aae8c11d80807012c1db
Erik Hebestreit <erik.hebestreit@gmail.com>
2018-06-09 14:00:59 +0200

    pretty index.php

--- index.php
+++ index.php
@@ -3,18 +3,13 @@ require ("view.header.php");
 ?>
 
 <html>
-<body >
-    <div class="container-fluid">
-        <div class="parent">
-            <h1 class="text-center">Tippspiel</h1>
-            <p class="text-center">Herzlich Willkommen beim Tippspiel</p>
-            <p class="text-center"><a  class="btn  btn-primary"  href="login.php">Log in  »</a></p>
-        </div>
+<body class="body-signin">
+<div class="container-fluid">
+    <div class="parent">
+        <h1 class="text-center">Tippspiel</h1>
+        <p class="text-center">Bitte loggen Sie sich zuerst ein!</p>
+        <p class="text-center"><a  class="btn  btn-primary"  href="login.php">Log in »</a></p>
     </div>
-
-<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" ></script >
-<script src = "https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" ></script >
-<script src = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
-        integrity = "sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin = "anonymous" ></script >
-</body >
+</div>
+</body>
 </html >
