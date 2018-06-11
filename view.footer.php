ff99d85ac57861ec68b7f3a090a621d7197d7013
Erik Hebestreit <erik.hebestreit@gmail.com>
2018-06-11 20:17:16 +0200

    cleanup java script files and loading

--- view.footer.php
+++ view.footer.php
@@ -1,2 +1,8 @@
+    <!--load java scripts at the end to speed up page load-->
+    <!--<script src="js/responsive-tables.js"></script>-->
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
